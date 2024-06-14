e209581c6bcaafa1040e0736aa8128a4eadc1deb
Ccocow <ccocow666@outlook.com>
2024-06-14 17:05:43 +0800

    项目初始化

---
+++ gulimall-product/src/test/java/cn/ccocow/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ccocow.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
